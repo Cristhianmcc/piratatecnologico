a3747c5457017bcad4393a26eb7f318964883ed4
Cristhianmca <cristhianmc84@gmail.com>
2025-06-18 11:33:31 -0500

    agregando la variable para enalzar el backend

--- src/utils/apiConfig.js
+++ src/utils/apiConfig.js
@@ -1,6 +1,14 @@
+// // Archivo de configuración para la API
+// const API_CONFIG = {
+//   BASE_URL: 'http://localhost:8080/api',
+//   TIMEOUT: 10000 // 10 segundos
+// };
+
+// export default API_CONFIG;
 // Archivo de configuración para la API
 const API_CONFIG = {
-  BASE_URL: 'http://localhost:8080/api',
+  // Usa la variable de entorno en producción, o localhost en desarrollo
+  BASE_URL: process.env.REACT_APP_API_URL || 'http://localhost:8080/api',
   TIMEOUT: 10000 // 10 segundos
 };
 
